872647596b66ac9f8f9b034107a5415195235f70
Gio d'Amelio <giodamelio@gmail.com>
2014-01-15 17:16:49 -0800

    Changed clock style

--- i3status_main.py
+++ i3status_main.py
@@ -9,7 +9,7 @@ status = Status(standalone=True)
 # Tue 30 Jul 11:59:46 PM KW31
 #                          ^-- calendar week
 status.register("clock",
-    format="%a %-d %b %X KW%V")
+    format="%a %-d %b %I:%M:%S")
 
 # Shows the average load of the last minute and the last 5 minutes
 # (the default value for format is used)
