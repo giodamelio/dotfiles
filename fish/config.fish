8ac939a458ceeb44222bfe43a110d64a5bcec653
Gio d'Amelio <giodamelio@gmail.com>
2014-07-28 19:57:21 -0700

    Set prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
+# Set the prompt
+source fish_prompt.fish
+
 # Set our editor
 set -U EDITOR vim
 
---
+++ fish/fish_prompt.fish
@@ -0,0 +1,63 @@
+function prompt_pwd --description 'Print the current working directory, NOT shortened to fit the prompt'
+    if test "$PWD" != "$HOME"
+        printf "%s" (echo $PWD|sed -e 's|/private||' -e "s|^$HOME|~|")
+    else
+        echo "~"
+    end
+
+end
+
+function fish_prompt
+    #### First line ####
+    set_color -o blue
+    printf "┌─["
+
+    # Username and hostname
+    set_color -o 555
+    printf "%s@%s" (whoami) (hostname -s)
+    set_color -o blue
+    printf "] ─ ["
+
+    # Working directory
+    set_color -o 555
+    printf "%s" (prompt_pwd)
+    set_color -o blue
+    printf "]"
+    echo
+    
+    #### Second line ####
+    # Git status
+    set -g __fish_git_prompt_show_informative_status 1
+    set -g __fish_git_prompt_hide_untrackedfiles 1
+
+    set -g __fish_git_prompt_color_branch magenta --bold
+    set -g __fish_git_prompt_showupstream "informative"
+    set -g __fish_git_prompt_char_upstream_ahead "↑"
+    set -g __fish_git_prompt_char_upstream_behind "↓"
+    set -g __fish_git_prompt_char_upstream_prefix ""
+
+    set -g __fish_git_prompt_char_stagedstate "●"
+    set -g __fish_git_prompt_char_dirtystate "✚"
+    set -g __fish_git_prompt_char_untrackedfiles "…"
+    set -g __fish_git_prompt_char_conflictedstate "✖"
+    set -g __fish_git_prompt_char_cleanstate "✔"
+
+    set -g __fish_git_prompt_color_dirtystate blue
+    set -g __fish_git_prompt_color_stagedstate yellow
+    set -g __fish_git_prompt_color_invalidstate red
+    set -g __fish_git_prompt_color_untrackedfiles blue
+    set -g __fish_git_prompt_color_cleanstate green --bold
+    printf "└─[%s" (__fish_git_prompt "%s")
+    set_color -o blue
+    printf "]"
+
+    # Regular user or root
+    set_color -o blue
+    if test (command whoami) = root
+        printf " ─ [%%] "
+    else
+        printf " ─ [\$] "
+    end
+    set_color normal
+end
+
