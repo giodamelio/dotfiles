5fe1f12a7ae296ae06f7bdb296d83e8bb026f701
Gio d'Amelio <giodamelio@gmail.com>
2014-07-29 16:54:45 -0700

    Fish: put prompt_pwd in its own file so it can actually work

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,12 +1,3 @@
-function prompt_pwd --description 'Print the current working directory, NOT shortened to fit the prompt'
-    if test "$PWD" != "$HOME"
-        printf "%s" (echo $PWD|sed -e 's|/private||' -e "s|^$HOME|~|")
-    else
-        echo "~"
-    end
-
-end
-
 function fish_prompt
     # Save the status of the last command
     set -l last_status $status
