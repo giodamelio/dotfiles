69f709bd69c04946fb5e5fe28c0c591648e6c8f9
Gio d'Amelio <giodamelio@gmail.com>
2014-08-03 23:34:44 -0700

    Fish: add funtion to setup up moniters

---
+++ fish/functions/moniter.fish
@@ -0,0 +1,11 @@
+function moniter
+    set vga (xrandr  | sed -n "s/^\(VGA-.\).*/\1/pg")
+    switch $argv[1]
+        case single
+            xrandr --output LVDS --primary --auto --output $vga --off
+        case dual
+            xrandr --output LVDS --auto --primary --output $vga --auto --right-of LVDS
+        case dual-left
+            xrandr --output LVDS --primary  --auto --output $vga --auto --left-of LVDS
+    end
+end
