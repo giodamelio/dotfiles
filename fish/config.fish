a066609a50c4cfd33ddaa564fd37ed4074ae56a6
Gio d'Amelio <giodamelio@gmail.com>
2014-10-13 22:21:51 -0700

    Fish: add gopath

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,11 @@
 set -U EDITOR vim
 
 # Append our bin path
-set -Ux PATH ~/.dotfiles/bin $PATH
+set -x PATH ~/.dotfiles/bin $PATH
+
+# Set GOPATH
+set -x GOPATH $HOME/Projects/go
+set -x PATH $GOPATH/bin $PATH
 
 # Use the fortune command as a greeting
 function fish_greeting
