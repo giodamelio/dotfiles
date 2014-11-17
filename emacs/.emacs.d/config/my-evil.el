23b50d66a464b86ec2c7a76b80364b008063195d
Gio d'Amelio <giodamelio@gmail.com>
2014-11-17 12:55:06 -0800

    Emacs: use ; as :

--- emacs/.emacs.d/config/my-evil.el
+++ emacs/.emacs.d/config/my-evil.el
@@ -10,6 +10,9 @@
     (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
     (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
     (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+    
+    ; Use ";" as ":"
+    (define-key evil-normal-state-map (kbd ";") 'evil-ex)
   ))
 
 ; Export it
