0a92dea5cfaf3cb80550ac0060867d22d3021a7f
Gio d'Amelio <giodamelio@gmail.com>
2014-11-16 19:18:24 -0800

    Emacs: easy window switching

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -14,3 +14,9 @@
 (require 'evil)
 (evil-mode 1)
 
+; Easy switching between windows
+(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+
