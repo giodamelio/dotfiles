054a9a35592a52cca936f660a1909531b2170120
Gio d'Amelio <giodamelio@gmail.com>
2015-03-29 05:56:17 -0700

    Awesome: switch terminal emulator to gnome-terminal and editor to vim

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -41,8 +41,8 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
-editor = os.getenv("EDITOR") or "nano"
+terminal = "gnome-terminal"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
