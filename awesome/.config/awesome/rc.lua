66c8942de7cd3db3fe7a4b75237b1fd5b1d66387
Gio d'Amelio <giodamelio@gmail.com>
2015-03-29 23:52:03 -0700

    Awesome: make meta-d run programs

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -261,7 +261,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
     -- Prompt
-    awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
+    awful.key({ modkey },            "d",     function () mypromptbox[mouse.screen]:run() end),
 
     awful.key({ modkey }, "x",
               function ()
