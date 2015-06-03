61d35b8b04db53fa7f8c3696da565541cec9005c
Gio d'Amelio <giodamelio@gmail.com>
2015-06-03 13:18:09 -0700

    Zsh: add some alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,3 +1,11 @@
 alias top=htop
 alias n=ncmpcpp
 
+# Make rm, cp and mv a bit safer
+alias rm='rm -I'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# Show progress on copy
+alias cpv="rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --"
+
