d0d66a815a019dae429bfbc8f6f497da5c2a9f8d
Gio d'Amelio <giodamelio@gmail.com>
2016-08-21 12:13:22 -0700

    Fish: remove old Elm path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,7 +11,6 @@ set -x PATH $PATH /usr/local/bin/
 set -x PATH $PATH /usr/bin/core_perl/  # Append perl bin to path
 set -x GOPATH $HOME/Projects/go # Setup GOPATH
 set -x PATH $GOPATH/bin $PATH
-set -x PATH $HOME/bin/elm/.cabal-sandbox/bin $PATH # Elm
 
 # Setup nvm wrapper
 source ~/.config/fish/nvm.fish
