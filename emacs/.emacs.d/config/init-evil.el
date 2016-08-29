94bd74668a85f8f614ddd972f2dba8100195f08e
Gio d'Amelio <giodamelio@gmail.com>
2016-08-28 18:27:26 -0700

    Emacs: install evil-leader and set leader to space

--- emacs/.emacs.d/config/init-evil.el
+++ emacs/.emacs.d/config/init-evil.el
@@ -1,5 +1,12 @@
 (require-package 'evil)
 (require 'evil)
+
+(require-package 'evil-leader)
+(require 'evil-leader)
+
+(evil-leader/set-leader "<SPC>") ; Set leader to space
+
+(global-evil-leader-mode)
 (evil-mode)
 
 ;; Make "jk" exit insert mode and exit a bunch of other modes
