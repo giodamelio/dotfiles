d2c366aa5e87f745514afd7804820e42ca790cb1
Gio d'Amelio <giodamelio@gmail.com>
2016-08-28 18:05:56 -0700

    Emacs: helm swap keybindings for persistent actions

--- emacs/.emacs.d/config/init-helm.el
+++ emacs/.emacs.d/config/init-helm.el
@@ -5,5 +5,8 @@
 (helm-mode 1) ; Enable helm mode to enhance common emacs commands
 
 (global-set-key (kbd "M-x") 'helm-M-x) ; Replace normal M-x with Helm's M-x
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; Rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; Make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; List actions using C-z
 
 (provide 'init-helm)
