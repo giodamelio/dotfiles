89e32e630a375ae739c15ee309d106a4eb902e26
Gio d'Amelio <giodamelio@gmail.com>
2016-08-28 17:05:48 -0700

    Emacs: use evil-escape plugin to escape all the buffers with 'jk'

--- emacs/.emacs.d/config/init-evil.el
+++ emacs/.emacs.d/config/init-evil.el
@@ -2,4 +2,10 @@
 (require 'evil)
 (evil-mode)
 
+;; Make "jk" exit insert mode and exit a bunch of other modes
+(require-package 'evil-escape)
+(require 'evil-escape)
+(evil-escape-mode)
+(setq evil-escape-key-sequence "jk")
+
 (provide 'init-evil)
