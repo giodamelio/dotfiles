9627ebb61c80548bc596100a65e049d0c5dd9834
Gio d'Amelio <giodamelio@gmail.com>
2016-08-29 15:14:03 -0700

    Emacs: install and enable relative line numbers

--- emacs/.emacs.d/config/init-eyecandy.el
+++ emacs/.emacs.d/config/init-eyecandy.el
@@ -1,8 +1,10 @@
 (use-package eyecandy
+  :ensure linum-relative
   :init
   (show-paren-mode) ; Highlight matching paren
   (global-linum-mode t) ; Show line numbers
   (global-hl-line-mode t) ; Highlight the current line
   (setq inhibit-startup-screen t) ; Disable startup screen
   (tool-bar-mode -1) ; Disable the toolbar
+  (linum-relative-global-mode) ; Enable relative line numbers
   )
