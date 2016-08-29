1ed45b6764658b31871bb9c276daee68f1a08e4e
Gio d'Amelio <giodamelio@gmail.com>
2016-08-28 20:21:23 -0700

    Emacs: rebind helm mini to leader-a and bind helm-find-files to leader-p

--- emacs/.emacs.d/config/init-helm.el
+++ emacs/.emacs.d/config/init-helm.el
@@ -9,11 +9,12 @@
   (setq helm-M-x-fuzzy-match t) ; Use fuzzy matching
 
   ;; Setup helm-mini
-  (evil-leader/set-key "p" 'helm-mini)
+  (evil-leader/set-key "a" 'helm-mini)
   (evil-leader/set-key "<SPC>" 'helm-mini)
   (setq helm-buffers-fuzzy-matching t
   helm-recentf-fuzzy-match    t)
 
+  (evil-leader/set-key "p" 'helm-find-files)
   (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; Rebind tab to run persistent action
   (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; Make TAB work in terminal
   (define-key helm-map (kbd "C-z")  'helm-select-action) ; List actions using C-z
