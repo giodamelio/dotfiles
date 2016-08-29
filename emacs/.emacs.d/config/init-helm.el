6e8a830d257bc8d1ff41a9cb31d41c59f83c5e03
Gio d'Amelio <giodamelio@gmail.com>
2016-08-28 18:08:34 -0700

    Emacs: helm enable fuzzy matching in helm-M-x

--- emacs/.emacs.d/config/init-helm.el
+++ emacs/.emacs.d/config/init-helm.el
@@ -4,7 +4,10 @@
 
 (helm-mode 1) ; Enable helm mode to enhance common emacs commands
 
-(global-set-key (kbd "M-x") 'helm-M-x) ; Replace normal M-x with Helm's M-x
+;; Replace normal M-x with Helm's M-x
+(global-set-key (kbd "M-x") 'helm-M-x)
+(setq helm-M-x-fuzzy-match t) ; Use fuzzy matching
+
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; Rebind tab to run persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; Make TAB work in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; List actions using C-z
