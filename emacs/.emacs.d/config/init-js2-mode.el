7809986476080387faef9e0aeed071c72c895a29
Gio d'Amelio <giodamelio@gmail.com>
2016-08-28 22:04:13 -0700

    Emacs: install js2-mode and enable it for all '*.js' files

---
+++ emacs/.emacs.d/config/init-js2-mode.el
@@ -0,0 +1,8 @@
+(use-package js2-mode
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (setq
+   js2-strict-missing-semi-warning nil ; Disable semi-colon warning
+   js2-basic-offset 2 ; Indent with two spaces
+  ))
