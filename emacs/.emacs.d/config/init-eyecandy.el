ba0869eb53ca369531e10fc2d153ee8496549104
Gio d'Amelio <giodamelio@gmail.com>
2016-08-28 17:58:25 -0700

    Emacs: disable startup screen

--- emacs/.emacs.d/config/init-eyecandy.el
+++ emacs/.emacs.d/config/init-eyecandy.el
@@ -1,5 +1,6 @@
 (show-paren-mode) ; Highlight matching paren
 (global-linum-mode t) ; Show line numbers
 (global-hl-line-mode t) ; Highlight the current line
+(setq inhibit-startup-screen t) ; Disable startup screen
 
 (provide 'init-eyecandy)
