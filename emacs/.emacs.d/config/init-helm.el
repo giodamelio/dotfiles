a7a5d679c84760219341bdc9d42e86a74d61db58
Gio d'Amelio <giodamelio@gmail.com>
2016-08-28 17:56:52 -0700

    Emacs: enable helm-mode to improve common emacs commands

--- emacs/.emacs.d/config/init-helm.el
+++ emacs/.emacs.d/config/init-helm.el
@@ -1,5 +1,8 @@
 (require-package 'helm)
+(require 'helm)
 (require 'helm-config)
+
+(helm-mode 1) ; Enable helm mode to enhance common emacs commands
 
 (global-set-key (kbd "M-x") 'helm-M-x) ; Replace normal M-x with Helm's M-x
 
