60ba3d795ac45ef15681e8d62d65826df86aadce
Gio d'Amelio <giodamelio@gmail.com>
2016-08-30 20:34:20 -0700

    Emacs: Add racer minor mode for rust auto complete and docs

--- emacs/.emacs.d/config/languages/rust.el
+++ emacs/.emacs.d/config/languages/rust.el
@@ -1,2 +1,17 @@
 (use-package rust
-  :ensure rust-mode)
+  :ensure rust-mode
+  :ensure racer
+  :after evil
+  :init
+  (setq
+   racer-rust-src-path "~/Projects/forks/rust/src" ; Tell racer where to find rust
+  )
+
+  ; Enable racer in rust buffers
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+
+  ; Bind some keys
+  (evil-leader/set-key
+    "h" 'racer-describe
+  ))
