aa526aa1c58e26198c56af1eee44937f0e61854c
Gio d'Amelio <giodamelio@gmail.com>
2016-09-11 12:59:52 -0700

    Emacs: install alchemist and add a keybinding for the docs

--- emacs/.emacs.d/config/languages/elixir.el
+++ emacs/.emacs.d/config/languages/elixir.el
@@ -1,2 +1,10 @@
-(use-package elixir
-             :ensure elixir-mode)
+(use-package elixir-mode
+  :ensure t)
+
+(use-package alchemist
+  :ensure t
+  :after elixir-mode
+  :after evil-leader
+  :init
+  (evil-leader/set-key-for-mode 'elixir-mode
+    "d" 'alchemist-help-search-at-point))
