8ba54a83c67893aa9c19f40bb714602104334a4d
Gio d'Amelio <giodamelio@gmail.com>
2016-09-11 13:31:18 -0700

    Emacs: add keybinding for cider-docs

--- emacs/.emacs.d/config/languages/clojure.el
+++ emacs/.emacs.d/config/languages/clojure.el
@@ -1,3 +1,10 @@
-(use-package clojure
-  :ensure clojure-mode
-  :ensure cider)
+(use-package clojure-mode
+  :ensure t)
+
+(use-package cider
+  :ensure t
+  :after clojure-mode
+  :after evil-leader
+  :init
+  (evil-leader/set-key-for-mode 'clojure-mode
+    "d" 'cider-doc))
