a71b926d8f122605963ab58de36b91bf02f22c24
Gio d'Amelio <giodamelio@gmail.com>
2016-09-11 13:34:59 -0700

    Emacs: make :W write file

--- emacs/.emacs.d/config/init-evil.el
+++ emacs/.emacs.d/config/init-evil.el
@@ -3,6 +3,9 @@
   :after evil-leader
   :config
   (evil-mode)
+  ;; Make :W act the same as w
+  (evil-ex-define-cmd "W" 'evil-write)
+  ;; Add some leader keybindings
   (evil-leader/set-key
     "c" 'comment-or-uncomment-region
     "k" 'kill-this-buffer
