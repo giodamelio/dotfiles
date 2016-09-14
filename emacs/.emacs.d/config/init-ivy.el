7fc1e487528b68d79f88588b69d1bd887729ae7f
Gio d'Amelio <giodamelio@gmail.com>
2016-09-13 18:53:01 -0700

    Emacs: make ivy 10 items tall. make ivy ignore search item order

--- emacs/.emacs.d/config/init-ivy.el
+++ emacs/.emacs.d/config/init-ivy.el
@@ -2,7 +2,13 @@
   :ensure t
   :diminish ivy-mode
   :config
-  (ivy-mode))
+  (ivy-mode)
+  ;; Make ivy display a few more items
+  (setq ivy-height 10)
+  ;; Configure ivy matching engine
+  (setq ivy-re-builders-alist
+	;; Allow searches to be out of order
+	'((t . ivy--regex-ignore-order))))
 
 (use-package counsel
   :ensure t
