7a396ec69a4640d34eface77ed69d89e644af0d7
Gio d'Amelio <giodamelio@gmail.com>
2016-10-24 17:29:24 -0700

    Lein: create a repl profile and inject aprint with lucid.core.inject

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -5,4 +5,18 @@
                   [lein-drip "0.1.1-SNAPSHOT"]
                   [lein-midje "3.1.3"]]
         :dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]]
-        :injections [(require '[clojure.tools.namespace.repl :refer [refresh]])]}}
+        :injections [(require '[clojure.tools.namespace.repl :refer [refresh]])]}
+
+ ;; A profile to make my repl do all kinds of awesome stuff
+ :repl {;; Dependencies used by the injections below
+        :dependencies [;; Inject stuff into a namespace
+                       [im.chit/lucid.core.inject "1.2.0"]
+                       ;; Print stuff really pretty
+                       [aprint "0.1.3"]]
+        ;; Everything inside this will be evaluated before every lein task expect "jar" and "uberjar"
+        :injections [(use 'lucid.core.inject)
+                     ;; Inject some stuff so it will be accessable everywhere
+                     (lucid.core.inject/in
+                      clojure.core >
+                      ;; aprint for pretty printing things
+                      [aprint.core aprint ap])]}}
