9280df1ce90d3164a26aca368f574dc88eb8bd90
Gio d'Amelio <giodamelio@gmail.com>
2017-01-28 15:15:15 -0800

    Bin: convert npmbin.fish to npmbin bash script

--- fish/.config/fish/functions/npmbin.fish
+++
@@ -1,8 +0,0 @@
-function npmbin
-  if test -d "./node_modules/.bin"
-    echo "Adding local npm bin to path"
-    set -x PATH (realpath "./node_modules/.bin/") $PATH
-  else
-    echo "Node modules folder does not exist"
-  end
-end
