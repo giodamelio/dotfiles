d7d1a17e4a81619a19492faf0ce9f50d21224b0c
Gio d'Amelio <giodamelio@gmail.com>
2017-02-06 11:01:11 -0800

    Fish: make zz function add path to next command line

--- fish/.config/fish/functions/zz.fish
+++ fish/.config/fish/functions/zz.fish
@@ -1,3 +1,7 @@
 function zz
-  z -l | tr -s " " | cut -d " " -f 2- | tail -n +2 | fzf
+  z -l | tr -s " " | cut -d " " -f 2- | tail -n +2 | fzf +s | read -l select
+  if not test -z $select
+    commandline -rb (printf $select)
+    commandline -f repaint
+  end
 end
