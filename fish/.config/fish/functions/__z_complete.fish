fe84c08bb5257804b42809bba9542a60bb3d8bc7
Gio d'Amelio <giodamelio@gmail.com>
2017-02-11 16:50:45 -0800

    Fish: remove z

--- fish/.config/fish/functions/__z_complete.fish
+++
@@ -1,1 +0,0 @@
-/home/giodamelio/.config/fisherman/z/functions/__z_complete.fish
--- fish/.config/fish/functions/zz.fish
+++
@@ -1,7 +0,0 @@
-function zz
-  z -l | tr -s " " | cut -d " " -f 2- | tail -n +2 | fzf +s | read -l select
-  if not test -z $select
-    commandline -rb (printf $select)
-    commandline -f repaint
-  end
-end
