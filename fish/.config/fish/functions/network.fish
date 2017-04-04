520099902735effb15e700424662f243fd591c02
Gio d'Amelio <giodamelio@gmail.com>
2017-04-03 23:11:25 -0700

    Fish: add GeoIP query into network command

--- fish/.config/fish/functions/network.fish
+++ fish/.config/fish/functions/network.fish
@@ -1,5 +1,15 @@
 function network
+    # Print network interfaces
     ip -4 -o a | cut -d ' ' -f 2,7 | cut -d '/' -f 1 | column -t | little_boxes --title "Network Interfaces"
+
+    # Print public ip addresses
     printf 'ipv4 %s\nipv6 %s\n' (curl --silent --ipv4 icanhazip.com) (curl --silent --ipv6 icanhazip.com)  | little_boxes --title "Public IP Address"
+
+    # Print location if "jq" command is present
+    if command --search jq > /dev/null do
+        printf 'location %s\n' (curl -s http://ipinfo.io/ | jq -r '.city + " " + .country') | little_boxes --title "GeoIP"
+    end
+
+    # Do a quick ping to google to test internet connectivity
     ping -c 4 -i 0.2 google.com | little_boxes --title "Ping Google"
 end
