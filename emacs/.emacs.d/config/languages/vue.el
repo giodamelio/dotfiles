51cfd8cc08d38fa91bdf53b4ab52adf2e110104e
Gio d'Amelio <giodamelio@gmail.com>
2017-06-12 11:56:16 -0700

    Emacs: Add helper function to run prettier on the JS part of a vue component.

--- emacs/.emacs.d/config/languages/vue.el
+++ emacs/.emacs.d/config/languages/vue.el
@@ -2,4 +2,16 @@
   :ensure vue-mode
   :mode ("\\.vue\\'" . vue-mode)
   :init
-  (setq mmm-submode-decoration-level 0))
+  (setq mmm-submode-decoration-level 0)
+  ;; Helper function to format the JS in a vue component
+  ;; Probably mostly broken and riddled with bugs
+  (defun prettier-vue ()
+  (interactive)
+  (let ((original (point)))
+    (goto-char 0)
+    (let* ((script-start (re-search-forward "<script>" nil t))
+           (start (+ script-start 1))
+           (script-end (re-search-forward "</script>" nil t))
+           (end (- script-end 10)))
+      (prettier-js--prettify start end)
+      (goto-char original)))))
