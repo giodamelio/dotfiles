50a878f9358b2cdcb35b6c038317a23e30258649
Gio d'Amelio <giodamelio@gmail.com>
2017-09-04 15:19:27 -0700

    Fish: change n alias to open nnn

--- fish/.config/fish/functions/n.fish
+++ fish/.config/fish/functions/n.fish
@@ -1,5 +1,10 @@
-# Shortcut for ncmpc -c
-function n
-    ncmpcpp
-end
+export NNN_TMPFILE="/tmp/nnn"
 
+function n --description 'nnn shortcut with cd capibility'
+  nnn $argv
+
+  if test -e $NNN_TMPFILE
+    . $NNN_TMPFILE
+    rm $NNN_TMPFILE
+  end
+end
