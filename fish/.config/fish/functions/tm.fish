b96aa1500f47ddc76df8da113e77e5fbb0e39769
Gio d'Amelio <giodamelio@gmail.com>
2017-10-31 14:56:31 -0700

    Fish: add tm function to easyly create, attach to and switch between tmux sessions

---
+++ fish/.config/fish/functions/tm.fish
@@ -0,0 +1,19 @@
+function tm
+  # Choose whether to switch sessions or connect to a session
+  if test -n "$TMUX"
+    set change "switch-client"
+  else
+    set change "attach-session"
+  end
+
+  # If a name is passed try to switch to it, or create it if it does not exist
+  if test (count $argv) -eq 1
+    tmux $change -t "$argv[1]" 2>/dev/null; or tmux new-session -s "$argv[1]"
+  else
+    # If no name is passes use fzf to select from current sessions
+    # If there is only one session select it by default (--select-1)
+    # If there is no sessions exit without prompting for fzf
+    tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf-tmux --select-1 --exit-0 | read -l session
+    tmux $change -t $session 2>/dev/null; or echo "No tmux sessions found"
+  end
+end
