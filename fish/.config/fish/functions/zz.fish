647bbeca7053da8683a784de299c81a4ef5b25ee
Gio d'Amelio <giodamelio@gmail.com>
2017-11-09 14:08:32 -0800

    Fish: update zz function to not cd to home on control-c and print messages

--- fish/.config/fish/functions/zz.fish
+++ fish/.config/fish/functions/zz.fish
@@ -1,4 +1,9 @@
 function zz
   fasd -d | fzf --no-sort --tac | tr -s " " | cut -d " " -f 2 | read -l dir
-  cd $dir
+  if test (count $dir) -gt 0
+    cd $dir
+    printf "Changed directory to \"%s\"\n" (pwd)
+  else
+    echo "No directory selected"
+  end
 end
