7aaa35e9f868861cc6edaf747a3f2e149ecad64f
Gio d'Amelio <giodamelio@gmail.com>
2018-01-03 10:11:24 -0800

    Bin: make tm script use fzf with a fixed height of half the terminal height instead of the slow fzf-tmux script

--- fish/.config/fish/functions/tm.fish
+++ fish/.config/fish/functions/tm.fish
@@ -13,7 +13,10 @@ function tm
     # If no name is passes use fzf to select from current sessions
     # If there is only one session select it by default (--select-1)
     # If there is no sessions exit without prompting for fzf
-    tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf-tmux --select-1 --exit-0 | read -l session
+    # TODO: check in on fzf-tmux
+    # Uses fzf --height <half of terminal height> because fzf-tmux is really slow right now
+    # See https://github.com/junegunn/fzf/issues/810
+    tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --height (math $LINES / 2) --select-1 --exit-0 | read -l session
     tmux $change -t $session 2>/dev/null; or echo "No tmux sessions found"
   end
 end
