c436b48f4f408c46024d33e9b6d463700dc2b420
Gio d'Amelio <giodamelio@gmail.com>
2018-03-08 13:39:16 -0800

    Fish: make disks script use require_commands.py script to ensure the commands it requires exist on the system

--- fish/.config/fish/functions/disks.fish
+++ fish/.config/fish/functions/disks.fish
@@ -1,4 +1,8 @@
 function disks
+  # Ensure the necessary commands are installed
+  require_commands.py --require all little_boxes --require linux lsblk --require darwin diskutil
+  if test $status -eq 1; return 1; end
+
   switch (uname)
     case Linux
       set command "lsblk -a"
