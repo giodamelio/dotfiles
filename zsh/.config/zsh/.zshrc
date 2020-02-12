e9dc3b0bcd3f499216c41679705c8960e32a5b00
Gio d'Amelio <giodamelio@gmail.com>
2020-02-11 17:14:56 -0800

    Zsh: setup Antibody

--- zsh/.config/zsh/.zshrc
+++ zsh/.config/zsh/.zshrc
@@ -2,3 +2,6 @@ source $ZDOTDIR/aliases.zsh # Include our aliases
 
 # Add some directories to the PATH
 export PATH=$HOME/bin:$PATH
+
+# Load Antibody plugins
+source ~/.zsh_plugins.sh
--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -5,3 +5,6 @@ alias ll='ls -l'
 # Make Vim point to Neovim
 alias vim='nvim'
 alias vimdiff='nvim -d'
+
+# Antigen
+alias antibody-refresh='antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh'
