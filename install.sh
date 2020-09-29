0287c5f0da26da622f5c50b877ca1a9993d6ae91
Gio d'Amelio <giodamelio@gmail.com>
2020-09-29 20:36:39 +0000

    Hack to make dotfiles work with Github Codespaces

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# Hack to make my dotfiles in Github Codespaces
+# Install stow and remove some conflicting config files
+if [ "$CODESPACES" == "true" ]; then
+    apt-get -y install --no-install-recommends stow
+    
+    rm ~/.gitconfig
+    rm ~/.bashrc
+fi
+
 stow */
 
