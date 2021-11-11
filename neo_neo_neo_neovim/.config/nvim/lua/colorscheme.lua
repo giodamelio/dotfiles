a8e0167e199bf2330b7b1600287efbfdd55707f2
Gio d'Amelio <giovanni@squareup.com>
2021-11-11 16:07:13 -0600

    Neovim: stop functions and comments being italic when tokyonight is enabled

--- neo_neo_neo_neovim/.config/nvim/lua/colorscheme.lua
+++ neo_neo_neo_neovim/.config/nvim/lua/colorscheme.lua
@@ -1,1 +1,6 @@
+-- Some tokyonight settings
+vim.g.tokyonight_italic_comments = false
+vim.g.tokyonight_italic_functions = false
+
+-- Set tokyonight as the default color scheme
 vim.cmd[[colorscheme tokyonight]]
