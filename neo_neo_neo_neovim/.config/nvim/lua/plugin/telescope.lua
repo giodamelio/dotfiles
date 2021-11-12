60d842c970af33d5600df34fd951503448ad9546
Gio d'Amelio <giovanni@squareup.com>
2021-11-12 15:27:15 -0600

    Neovim: Setup Telescope to open results in Trouble

---
+++ neo_neo_neo_neovim/.config/nvim/lua/plugin/telescope.lua
@@ -0,0 +1,11 @@
+local telescope = require("telescope")
+local trouble = require("trouble.providers.telescope")
+
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = { ["<c-t>"] = trouble.open_with_trouble },
+      n = { ["<c-t>"] = trouble.open_with_trouble },
+    },
+  },
+})
